45c06af7da89916076e3d359cddf6847161f1970
Nahuel Pacheco <pachecoalannahuel@gmail.com>
2020-08-12 01:21:39 -0300

    .

---
+++ Bola_magica.py
@@ -0,0 +1,20 @@
+import random
+
+respuestas=["En mi opinión, sí", "Es cierto", "Es decididamente así", "Probablemente","Buen pronóstico", "Todo apunta a que sí",
+            "Sí","Sí - definitivamente", "Debes confiar en ello", "Respuesta vaga, vuelve a intentarlo","Pregunta en otro momento", 
+            "Será mejor que no te lo diga ahora", "No puedo predecirlo ahora", "Concéntrate y vuelve a preguntar", "Puede ser",
+            "No cuentes con ello","Mi respuesta es no","Mis fuentes me dicen que no","Las perspectivas no son buenas", "Muy dudoso"]
+entrada_o_salida=input("Presiona ENTER para agitar la bola magica. Escribe SALIR para salir: ")
+
+if(entrada_o_salida.lower()=="salir"):
+    print("Nos vemos")
+elif (entrada_o_salida!=""):
+    print("No ingresaste algo valido")
+    entrada_o_salida=input("Presiona ENTER para agitar la bola magica. Escribe SALIR para salir: ")
+
+
+while(entrada_o_salida==""):
+    pregunta=input("Cual es tu pregunta? \n")
+    print(respuestas[random.randint(0,20)])
+    entrada_o_salida=input("Presiona ENTER para agitar la bola magica. Escribe SALIR para salir: ")
+
